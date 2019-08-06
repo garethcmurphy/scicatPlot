399baeedbd703f75b523b352f7ac67b11263e6b4
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-06 11:57:51 +0200

    add attach skeleton

---
+++ src/scicatAttach.py
@@ -0,0 +1,25 @@
+"""attach image to scicat"""
+
+
+class ScicatAttach:
+    """attach image to scicat"""
+
+    def __init__(self):
+        pass
+
+    def login(self):
+        """login to scicat"""
+        pass
+
+    def attach(self):
+        """attach image to scicat"""
+        pass
+
+
+def main():
+    """attach image to scicat"""
+    ScicatAttach()
+
+
+if __name__ == "__main__":
+    main()
