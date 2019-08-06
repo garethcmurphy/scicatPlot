c4afde50ca2b48f4426fe5a68afa926d3e282693
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-06 11:38:14 +0200

    first commit

---
+++ scicatPlot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+class scicatPlot:
+    def __init__(self):
+        pass
+
+    def plot(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ scicatSearch.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import requests
+import json
+import urllib
+
+def search_scicat(text, max_number_results):
+    fields = {'text': text}
+    limit = {'limit': max_number_results, 'order': "creationTime:desc"}
+    fields_encode = urllib.parse.quote(json.dumps(fields))
+    limit_encode = urllib.parse.quote(json.dumps(limit))
+    dataset_url = "https://scicatapi.esss.dk/api/v3/Datasets/anonymousquery?fields=" + \
+                fields_encode+"&limits="+limit_encode
+    r=requests.get(dataset_url).json()
+    print(len(r), "result found!")
+    return r
+
+
+if __name__ == "__main__":
+    r=search_scicat("nicos_00000490",1)
+    result = r.pop()
+    print(result["pid"])
+    print(result["pid"])
+    pass
