49f4010a211f8b3b38cfd7cd2bdc7911c14513c5
Gareth Murphy <garethcmurphy@gmail.com>
2019-09-10 13:28:23 +0200

    add error

--- src/scicat_met.py
+++ src/scicat_met.py
@@ -13,64 +13,65 @@ class ScicatMet:
         self.metadata_dict = {}
         self.file_name = "data/nicos_00000332.hdf"
         self.file_name = "data/nicos_00000490.hdf"
+        self.file = ""
         print("test")
 
     def set_filename(self, file_name):
         """setter for filename"""
         self.file_name = file_name
 
-    def get_dataset(self, file, path):
+    def get_dataset(self,  key, path):
         """get dataset from file"""
         val = ""
-        if path in file:
-            val = file[path][()]
+        if path in self.file:
+            val = self.file[path][()]
             base = os.path.basename(path)
-            self.metadata_dict[base] = val
-            print(path, val)
-        else:
-            print("path missing")
-            return 0
-        return val
-    
-    def get_array(self, file, path):
-        """get dataset from file"""
-        val = ""
-        if path in file:
-            val = file[path][()]
-            base = os.path.basename(path)
-            self.metadata_dict[base] = val[:]
+            self.metadata_dict[key] = val
             print(path, val)
         else:
             print("path missing")
             return 0
         return val
 
+    def get_array(self, path):
+        """get dataset from file"""
+        val = ""
+        if path in self.file:
+            val = self.file[path][()]
+            base = os.path.basename(path)
+            self.metadata_dict[base] = val[:]
+            print(path, val)
+        else:
+            print("path missing", path)
+            return 0
+        return val
 
     def get_metadata(self):
         """read nexus file"""
         if not h5py.is_hdf5(self.file_name):
             print("Invalid file", self.file_name)
             return 0
-        file = h5py.File(self.file_name, "r")
-        event_path = "/entry/monitor_1/events/event_id"
-        if event_path in file:
-            pass
-        else:
-            print("path missing")
-            return 0
+        self.file = h5py.File(self.file_name, "r")
 
         path = "/entry/title"
-        self.get_dataset(file, path)
+        self.get_dataset("title", path)
         path = "/entry/sample/description"
-        self.get_dataset(file, path)
+        self.get_dataset("sample_description", path)
         path = "/entry/start_time"
-        self.get_dataset(file, path)
-        path = "/entry/instrument/tilting_angle_2/velocity/value"
-        self.get_array(file, path)
+        self.get_dataset("start_time", path)
+        path = "/file_name"
+        self.get_dataset("start_time", path)
+
+        for i in range(9):
+            path = "/entry/instrument/chopper_"+str(i)+"/radius"
+            self.get_dataset("chopper_"+str(i)+"_radius", path)
+
+        #path = "/entry/instrument/tilting_angle_2/velocity/value"
+        # self.get_array(path)
 
         print("\n\n")
-        pp=pprint.PrettyPrinter(indent=4)
-        pp.pprint(self.metadata_dict)
+        printer = pprint.PrettyPrinter(indent=4)
+        printer.pprint(self.metadata_dict)
 
 
 def main():
