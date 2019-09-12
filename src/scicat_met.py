dceeab37422e6903ab27381bcde6b575ccf81a32
Gareth Murphy <garethcmurphy@gmail.com>
2019-09-12 14:48:00 +0200

    fix typo

--- src/scicat_met.py
+++ src/scicat_met.py
@@ -124,7 +124,7 @@ class ScicatMet:
         print(updated_metadata)
         api = GetApi()
         api = api.get()
-        assert len(token), 64
+        assert len(token) == 64
         # token = "Gkbbx3RT2dzCgmuoqUnwQCdhmvGbukAQcI2onZD3K6j6mywXQrnHVdQPGh2Qw18W"
         url = api + "api/v3/Datasets/updateScientificMetadata?access_token="+token
         print(url)
